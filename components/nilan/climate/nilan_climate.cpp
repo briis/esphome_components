612e35440865569497fcf7a7200fa779c444321f
Bjarne Riis <bjarne@briis.com>
2026-05-21 13:57:30 +0200

    Fix set_supported_custom_fan_modes call syntax

--- components/nilan/climate/nilan_climate.cpp
+++ components/nilan/climate/nilan_climate.cpp
@@ -91,7 +91,7 @@ void NilanClimate::control(const climate::ClimateCall& call) {
 climate::ClimateTraits NilanClimate::traits() {
   auto traits = climate::ClimateTraits();
 
-  traits.set_supported_custom_fan_modes({
+  set_supported_custom_fan_modes({
     "1",
     "2",
     "3",
